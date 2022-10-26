9a8518c13b8646e520e58a5f9da8711599f23b2f
babygotbrooke <neshamarovinsky@gmail.com>
2022-10-26 14:13:25 -0400

    project start

---
+++ PrintsPlentyOne/src/main/java/com/collab/PrintsPlentyOne/Services/PrintsPlentyOneApplication.java
@@ -0,0 +1,13 @@
+package com.collab.PrintsPlentyOne.Services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintsPlentyOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintsPlentyOneApplication.class, args);
+	}
+
+}
---
+++ PrintsPlentyOne/src/test/java/com/collab/PrintsPlentyOne/PrintsPlentyOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collab.PrintsPlentyOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintsPlentyOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
